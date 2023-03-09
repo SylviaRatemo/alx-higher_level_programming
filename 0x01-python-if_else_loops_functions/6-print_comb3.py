97e03e31945a994597d3566481685d140bd0e43b
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-08 18:06:12 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for num in range (0, 10):
-    for num2 in range (num + 1, 10):
+for num in range(0, 10):
+    for num2 in range(num + 1, 10):
         if num == 8 and num2 == 9:
             print("{}{}".format(num, num2))
         else:
