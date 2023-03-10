7426a5b06b9247985da49f9a8e8bb9033a32e86b
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-09 16:17:06 -0800

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,5 +1,12 @@
 #!/src/bin/python3
-def no_args(*args):
-    while(args):
-        print("{}: {}".format((len(args), args)))
-        args = args + 1
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.args[i + 1)))
