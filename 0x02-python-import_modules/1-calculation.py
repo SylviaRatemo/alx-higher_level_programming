0d5b83c9d6da8d02c5b7e76135daecef2c302822
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-09 16:12:32 -0800

    Task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as mod
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, mod.add(a, b)))
-print("{} - {} = {}".format(a, b, mod.sub(a, b)))
-print("{} * {} = {}".format(a, b, mod.mul(a, b)))
-print("{} / {} = {}".format(a, b, mod.div(a, b)))
+if __name__ == "__main__":
+    import calculator_1 as mod
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, mod.add(a, b)))
+    print("{} - {} = {}".format(a, b, mod.sub(a, b)))
+    print("{} * {} = {}".format(a, b, mod.mul(a, b)))
+    print("{} / {} = {}".format(a, b, mod.div(a, b)))
