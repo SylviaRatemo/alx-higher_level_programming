9e15c9fc118a7fba510d0c1755bf30c4ff490bb5
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-13 08:01:03 -0700

    Task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
+    for i in range(len(my_list) - 1):
         if mylist[i].isdigit():
             print("{}".format(my_list[i]))
             print()
