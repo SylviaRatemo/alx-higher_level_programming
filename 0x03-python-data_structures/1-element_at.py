ba6895da69584d2ecd998c280feed23627b94716
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-13 08:43:21 -0700

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,6 +4,6 @@ def element_at(my_list, idx):
         return (None)
     if idx > (len(my_list)):
         return (None)
-    for i in my_list:
+    for i in range(len(my_list)):
         if my_list[i] == idx:
             return (my_list[i])
