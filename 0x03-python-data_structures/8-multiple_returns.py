11d9a0eb19824d0786be054ca87d9078e4a6da36
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-14 01:19:29 -0700

    Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!usr/bin/python3
 def multiple_returns(sentence):
     if sentence == "":
-        sentence[0] = None
+        sentence += " " 
     new_t = (len(sentence), sentence[0])
     return (new_t)
