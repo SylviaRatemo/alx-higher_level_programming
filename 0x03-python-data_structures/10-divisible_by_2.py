d2175fbc30130eed545eeac3c51cc57c6e8baf70
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-14 02:34:46 -0700

    Task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     check = []
     for x in range(len(my_list)):
         if my_list[x] % 2 == 0:
-            check.append("True")
+            check.append(True)
         else:
-            check.append("False")
+            check.append(False)
     print(check)
