e20bca0e50318bbc88e449a62d7269468581f26c
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-13 17:57:15 -0700

    Task 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    res=[]
+    for i in range(o, len(tuple_a)):
+        res.append(tuple_a[i]+tuple_b[i])
+    res = tuple(res)
+    print(str(res))
