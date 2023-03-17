38bc1d2c749733b575501ef06426751ed4f04823
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-17 00:53:03 -0700

    Error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    new_dict = sorted(a_dictionary.items())
-    for key, value in new_dict:
+    for key, value in sorted(a_dictionary.items()):
         print("{}: {}".format(key, value))
