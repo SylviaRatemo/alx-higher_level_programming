7cdf3b51960de137cafdeafd9d9c42de3f313658
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-18 11:45:45 +0300

    Update main.py

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-complex_delete = __import__('102-complex_delete').complex_delete
-print_sorted_dictionary = \
-    __import__('6-print_sorted_dictionary').print_sorted_dictionary
+uppercase = __import__('8-uppercase').uppercase
 
-a_dictionary = {'lang': "C", 'track': "Low", 'pref': "C", 'ids': [1, 2, 3]}
-new_dict = complex_delete(a_dictionary, 'C')
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
-
-print("--")
-print("--")
-new_dict = complex_delete(a_dictionary, 'c_is_fun')
-print_sorted_dictionary(a_dictionary)
-print("--")
-print_sorted_dictionary(new_dict)
+uppercase("best")
+uppercase("Best School 98 Battery street")
