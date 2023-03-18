67dd5f4050546a1f8ad383200c3d519fb8645c8d
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-17 18:30:00 -0700

    Advanced

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def weighted_average(my_list=[]):
-
+    numerator = 0
+    if my_list == NULL:
+        return 0
+    for x in my_list:
+        for y in my_list:
+            numerator += (x * y)
+            y += y
+    return numerator / y
