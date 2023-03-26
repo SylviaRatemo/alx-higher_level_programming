22a2a22bddc21b0a482d31882ce8f86ecfd9e507
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-26 08:00:00 -0700

    Revision

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
+    if number <= 0:
         number *= -1
-    rem = number % 10
-    if rem == 0:
-        print(0)
-    else:
-        print(rem)
+    print(number % 10)
