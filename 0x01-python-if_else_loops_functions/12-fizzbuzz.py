d63224f7bb120cb9d5b8e7139ee9a907e324b6a0
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-26 08:28:15 -0700

    Revision

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    n = 0
+    while (n < 101):
+        if (n % 3 == 0 && n % 5 != 0):
+            print("Fizz", end=" ")
+        elif (n % 3 == 0 && n % 5 == 0):
+            print("FizzBuzz", end=" ")
+        elif (n = 100)
+            print(n)
+        else:
+            print(n, end=" ")
+        n += 1
+
+
