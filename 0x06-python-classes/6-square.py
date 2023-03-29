8c7d8b1ea5f236a259822e0a173d35141c00c5e9
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-29 08:07:01 -0700

    Task 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Define a class based on 5-square.py."""
+
+
+class Square:
+    """Representation of a square."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialization function.
+        Attributes:
+            size (int): size of the square
+            position (tuple): tuple of 2 positive integers
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Gettor function.
+        Returns:
+            The size of the square.
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Settor function.
+        Attributes:
+            size (int): size of the square
+        Raises:
+            ValueError: If size < 0
+            TypeError: If size is not an int
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def position(self):
+        """Position settor function."""
+        return (self.__position)
+
+    def position(self, value):
+        """Position gettor function."""
+        pos = self.__position
+        check = list(filter(lambda sub: all(pos >= 0 for pos), value))
+        if check == NULL:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    def area(self):
+        """Public function compute area.
+        Returns:
+            The current square area
+        """
+        area = self.__size ** 2
+        return (area)
+
+    def my_print(self):
+        """Funtion to print in stdout the square with #."""
+        if self.__size == 0:
+            print()
+        for i in range(0, self.__size):
+            [print("#", end="") for k in range(self.__size)]
+            print()
