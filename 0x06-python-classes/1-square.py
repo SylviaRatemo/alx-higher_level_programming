7604bcff6686f7272848e7dae02fa06142cab6b8
SylviaRatemo <ratemosylvia@gmail.com>
2023-03-28 23:19:53 -0700

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""Class based on 0-square.py"""
 class Square:
+    """initialization function
+
+    Attributes:
+        size (int): size of the square
+    """
     def __init__(self, size):
         self.__size = size
