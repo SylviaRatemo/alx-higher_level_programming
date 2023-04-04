5d8676372323de5defa499c02353ecbf2ce0e003
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-04 00:31:55 -0700

    Error

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -53,7 +53,7 @@ class Rectangle:
         """
 
         if not isinstance(value, int):
-            raise TypeError("width must be an int")
+            raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
