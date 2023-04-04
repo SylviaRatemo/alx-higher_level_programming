3d489c9d3a4c0c0f222c5d87c89a66bd5056a6c0
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-04 02:26:24 -0700

    Pycodestyle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,6 +6,7 @@ class Rectangle:
     """Representation of a rectangle"""
 
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialization function
         Attributes:
