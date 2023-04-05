db1e824e34230887a13672c4b64d6c4807be4724
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-04 19:00:38 -0700

    100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -2,4 +2,4 @@
 def magic_string():
     magic_string.counter = getattr(magic_string, "counter", 0) + 1
 
-    return ", ".join(["Holberton" for i in range(magic_string.counter)])
+    return ", ".join(["BestSchool" for i in range(magic_string.counter)])
