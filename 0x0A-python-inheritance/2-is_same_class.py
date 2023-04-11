1fafff62b6696c74f62585519b03adac8c389814
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-11 04:56:55 -0700

    check type

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Function to check object"""
+
+
+def is_same_class(obj, a_class):
+    """Function definition for is_same_class"""
+
+    if type(obj) is a_class:
+        return True
+    return False
+    
