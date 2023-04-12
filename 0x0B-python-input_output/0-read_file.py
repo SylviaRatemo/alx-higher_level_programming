992fc39091ea737f2dc50ef327ba23cb16abfb25
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-04-12 22:50:33 +0000

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function to read text file (UTF8)"""
+
+
+def read_file(filename=""):
+	"""Function definition"""
+
+	with open(filename, mode="r", encoding="utf-8") as myFile:
+		print(myFile.read())
