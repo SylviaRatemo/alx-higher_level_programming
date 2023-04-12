5e4d83a5322fe2e2068efaa085d67d576459273b
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-12 16:27:29 -0700

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function to return JSON representation of an object(string)"""
+import json
+
+
+def to_json_string(my_obj):
+    """Function Definition"""
+
+    return json.dumps(my_obj)
