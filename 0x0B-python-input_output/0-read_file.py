33ff9214c49a322c10f52f6a78165167db5892db
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-12 16:00:46 -0700

    Task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Function definition"""
 
     with open(filename, mode="r", encoding="utf-8") as myFile:
-        print(myFile.read())
+        print(myFile.read(), end="")
