d01236595cc51c118bba85cd8fa0cc58544f98f2
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-12 16:11:08 -0700

    Task 2

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """Function definition"""
 
     with open(filename, mode="w", encoding="utf-8") as myFile:
-        myFile.write(text)
+        return myFile.write(text)
