b993eea7ae86527bc9f6075bd815767d1639bd01
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-12 17:05:28 -0700

    Task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Add arguments to a python list"""
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 import sys
 import json
 import os
+"""Add arguments to a python list"""
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 arg_list = []
 
