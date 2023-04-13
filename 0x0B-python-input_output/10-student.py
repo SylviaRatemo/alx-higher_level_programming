1bce422a89a0c86c81b1ffb849e00ac4b3ca2e72
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-12 17:46:10 -0700

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Class student"""
+
+
+class Student:
+    """Class student descriptio"""
+
+    def __init__(self, first_name="", last_name="", age=0):
+        """Class constructor
+        Attributes:
+            first_name (str): First name
+            last_name (str): Last name
+            age (int): Student age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Get dictionary of student"""
+        if (type(attrs) == list and all(type(i) == str for i in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
