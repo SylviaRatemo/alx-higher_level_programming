d3ece92068a9cbc72b51e3cabe1492a4ecf76268
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-20 13:29:49 -0700

    Redo

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""""First class Base"""
+
+class Base:
+    """"Class Base Description"""
+
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor method"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
