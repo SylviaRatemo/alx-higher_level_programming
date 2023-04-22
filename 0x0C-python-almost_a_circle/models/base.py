1f0dd417dcf99384b1d35ca6b01954735531062e
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-21 22:36:12 -0700

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """"First class Base"""
+import json
 
 
 class Base:
@@ -14,3 +15,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """returns JSON sring"""
+
+        return json.dumps(list_dictionaries)
