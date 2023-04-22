0d72b682c6e9d88bd214714ae3e1f4007ef010ec
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-21 22:42:19 -0700

    static methid

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,7 +16,10 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns JSON sring"""
 
+        if list_dictionaries is None:
+            return "[]"
         return json.dumps(list_dictionaries)
