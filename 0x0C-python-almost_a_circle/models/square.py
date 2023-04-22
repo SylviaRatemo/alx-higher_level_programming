94a1c13f399e221558514a224e9969548613708b
SylviaRatemo <ratemosylvia@gmail.com>
2023-04-21 22:31:07 -0700

    dict s

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -60,8 +60,8 @@ class Square(Rectangle):
                     self.x = value
                 elif key == "y":
                     self.y = value
-    
+
     def to_dictionary(self):
         """dictionary representation of Square"""
 
-        return super().to_dictionary()
+        return {"id": self.id, "size": self.width, "x": self.x, "y": self.y}
