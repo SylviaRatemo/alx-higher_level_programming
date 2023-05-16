4bd3c2e96e02a0f23da3a04f62f0f8dd9c3c79c6
SylviaRatemo <ratemosylvia@gmail.com>
2023-05-16 16:45:34 -0700

    temp #0

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- avg temp
-SELECT city, AVG(value) AS "avg_temp" FROM temperatures GROUP BY city ORDER BY value DESC;
+SELECT city, AVG(value) AS "avg_temp" FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
 
