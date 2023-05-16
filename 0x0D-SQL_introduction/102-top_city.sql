1248d7c05343a57a02e777ea31e48df991ee15d2
SylviaRatemo <ratemosylvia@gmail.com>
2023-05-16 16:44:02 -0700

    temp #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- top cities
-SELECT city, AVG(value) AS "avg_temp" FROM temperatures WHERE month = 7 or month = 8 GROUP BY city ORDER BY value DESC LIMIT 3;
+SELECT city, AVG(value) AS "avg_temp" FROM temperatures WHERE month = 7 or month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
 
