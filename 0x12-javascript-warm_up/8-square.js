d957d2d194dd82994900ed3040344a9e75afe2a6
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-12 06:11:07 -0700

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const x = parseInt(process.argv[2]);
+if (!isNaN(x)) {
+  for (let i = 0; i < x; i++) {
+    console.log('X'.repeat(x));
+  }
+} else { console.log('Missing size'); }
