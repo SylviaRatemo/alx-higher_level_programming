f3c7f2c3e3ad88f8f27d838b66897ccb807be3ea
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-13 11:18:35 -0700

    Task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) { if (w === 0 || h === 0) { const obj = new Object(); } this.width = w; this.height = h; }
+  constructor (w, h) { if (w > 0 && h > 0) { this.width = w; this.height = h; } }
 }
 module.exports = Rectangle;
