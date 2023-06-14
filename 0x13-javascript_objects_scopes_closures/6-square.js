b1735ee852669802838a330f47dc9958160c3695
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-14 04:49:23 -0700

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const parentSquare = require('./5-square');
+class Square extends parentSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    process.stdout.write((c.repeat(this.width) + '\n').repeat(this.height));
+  }
+}
+
+module.exports = Square;
