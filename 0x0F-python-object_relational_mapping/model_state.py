fee18c15d054e76e125aa8949a23bdec0207d742
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-17 12:02:18 -0700

    Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,6 @@
 import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
