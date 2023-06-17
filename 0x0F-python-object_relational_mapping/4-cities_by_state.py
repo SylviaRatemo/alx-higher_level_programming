bf5602ef1ecabb2b809ea968da6e15a7148e2d6d
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-17 08:22:40 -0700

    Task 4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Lists all states with N*"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                                 user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = connection.cursor()
+
+    sql = """
+    SELECT t1.id, t2.name, t1.name
+    FROM cities t1
+    JOIN states t2 ON t1.state_id = t2.id"""
+    cursor.execute(sql)
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    connection.close()
