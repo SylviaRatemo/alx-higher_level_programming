101dbfe68481667980d80c2602db7cb985282c76
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-17 08:27:55 -0700

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     sql = """
     SELECT t1.id, t2.name, t1.name
     FROM cities t1
-    JOIN states t2 ON t1.state_id = t2.id"""
+    JOIN states t2 ON t1.state_id = t2.id ORDER BY t1.id ASC"""
     cursor.execute(sql)
     query_rows = cursor.fetchall()
     for row in query_rows:
