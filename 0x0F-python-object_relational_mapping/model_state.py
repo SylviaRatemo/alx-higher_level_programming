103f75a8372a98c6cef726f91ff5d566a98b1c9a
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-17 10:56:20 -0700

    Task 6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,7 @@
 import sys
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -13,13 +13,7 @@ Base = declarative_base()
 class State(Base):
     """State class inherits from Base"""
 
-    table = 'states'
-    id = Column(Integer, primary_keys=True, autoincrement=True)
+    __table__ = 'states'
+    id = Column(Integer, primary_keys=True, autoincrement=True,
+                nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-
-
-if __name__ == "___main__":
-    engine = create_engine('mysql+mysqldb : //{} : {}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    Base.metadata.create_all(engine)
