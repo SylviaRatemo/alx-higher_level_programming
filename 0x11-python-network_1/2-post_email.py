234dd35cf4748aef6e39a32ec05fd7e36961d78e
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-30 10:44:16 -0700

    Task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""urllib training"""
+import urllib.parse as p
+import urllib.request as r
+import sys
+
+
+if __name__ == "__main__":
+    data = p.urlencode({"email": sys.argv[2]}).encode()
+    with r.urlopen(sys.argv[1], data) as response:
+        html = response.read()
+        print("Your email is: {}".format(html.decode('utf-8')))
