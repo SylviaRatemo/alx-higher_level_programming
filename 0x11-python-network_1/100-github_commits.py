ce7d3febf2327a5077d433609178d489cc60256e
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-30 14:26:10 -0700

    Task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         if count < 10:
             print("{}: {}".format(i.get("sha"),
                   i.get("commit").get("author").get("name")))
-        count+=1
+        count += 1
