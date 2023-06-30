5544cdccc902e01667f423274a1ede2e158150d5
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-30 13:45:13 -0700

    Task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""requests training"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    data = {"q": sys.argv[1][0] if len(sys.argv) > 1 else ""}
+    r = requests.post("http://0.0.0.0:5000/search_user", data=data)
+    try:
+        r = r.json()
+        if not r:
+            print("No result")
+        else:
+            print("[{}] {}".format(r.get("id"), r.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
