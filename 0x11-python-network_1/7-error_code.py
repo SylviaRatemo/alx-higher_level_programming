40880d7cd229785874982c5e1dc7746461cf078e
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-30 12:51:31 -0700

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,4 +6,5 @@ import sys
 
 if __name__ == "__main__":
     r = requests.get(sys.argv[1])
-    print("Error code: {}".formmat(r.status_code))
+    if r.status_code >= 400:
+        print("Error code: {}".formmat(r.status_code))
