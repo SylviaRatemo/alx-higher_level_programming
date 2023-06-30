8c720e5dfb18168898f36a4761369b4851484d56
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-30 05:12:37 -0700

    Task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # curl get size in bytes
-curl -sS -X POST -d "email="test@gmail.com"&subject="I will always be here for PLD"" "$1"
+echo "POST params:\n"
+curl -sS -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
