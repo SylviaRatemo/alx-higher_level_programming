46ed85426bf54d49f9b54b6e31159577eef2bc4c
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-30 01:20:37 -0700

    Task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,18 +1,3 @@
 #!/usr/bin/env bash
-# take URL, send request, display size of body
-
-if [$# -eq 0]; then
-	echo "Usage: $0 [Insert URL]"
-	exit 1
-fi
-
-url=$1
-response=$(curl -sI "http://$url")
-content_length=$(echo "$response" | awk '/Content-Length/ { print $2 }')
-
-if [ -z "$content_length" ]; then
-	echo "Error: Length not found"
-	exit 1
-fi
-
-echo "$content_length"
+# curl get size in bytes
+curl -so /dev/null -w '%{size_download}' "$1"
