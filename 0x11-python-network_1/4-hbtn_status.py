9455fdaf8fbf91e3099a160fc5dd2e793825de16
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-30 11:08:12 -0700

    Task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,5 @@ import urllib.request as url
 
 
 if __name__ == "__main__":
-    with url.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:\n\t- type: {}\n\t- content: {}"
-              .format(type(html.text), html.text))
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(r.text), r.text))
