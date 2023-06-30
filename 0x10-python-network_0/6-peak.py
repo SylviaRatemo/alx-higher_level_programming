11feb8ea6a7883e9768cc39373f939b5bf8b3e0e
SylviaRatemo <ratemosylvia@gmail.com>
2023-06-30 16:08:38 +0300

    PY

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,12 +3,24 @@
 
 
 def find_peak(list_of_integers):
-    COUNT = 0
-    HIGH = 0
-    for i in list_of_integers:
-        if i > HIGH:
-            HIGH = i
-            COUNT += 1
-    if COUNT < 1:
+    """Finds peak in unsorted list of integers"""
+
+    if len(list_of_integers) > 1:
+        if list_of_integers[0] >= list_of_integers[1]:
+            return list_of_integers[0]
+        if list_of_integers[-1] >= list_of_integers[-2]:
+            return list_of_integers[-1]
+        return peak_recurse(list_of_integers, 0, len(list_of_integers))
+    if not list_of_integers:
         return None
-    return HIGH
+    return list_of_integers[0]
+
+
+def peak_recurse(LO, start, stop):
+    """Recursively finds peak"""
+    if stop - start < 2:
+        return None
+    mid = (start + stop) // 2
+    if LO[mid] >= LO[mid - 1] and LO[mid] >= LO[mid + 1]:
+        return LO[mid]
+    return peak_recurse(LO, start, mid) or peak_recurse(LO, mid, stop)
