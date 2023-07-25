f027a7ead29f84b0ec680f5cc46d8c9a886f2d6a
SylviaRatemo <ratemosylvia@gmail.com>
2023-07-25 11:10:00 -0700

    Star wars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,17 +1,30 @@
 #!/usr/bin/node
-//star wars
 const request = require('request');
 
-const Id = process.argv[2];
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${Id}`;
+function fetchMovieTitle(movieId) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+  
+  request(url, (err, res, body) => {
+    if (err) {
+      console.error('Error:', err);
+    } else if (res.statusCode !== 200) {
+      console.error('Error: Unexpected status code:', res.statusCode);
+    } else {
+      try {
+        const data = JSON.parse(body);
+        console.log(data.title);
+      } catch (error) {
+        console.error('Error parsing JSON:', error.message);
+      }
+    }
+  });
+}
 
+// Check if a movie ID is provided as a command-line argument
+if (process.argv.length < 3) {
+  console.error('Please provide the movie ID as a command-line argument.');
+} else {
+  const movieId = process.argv[2];
+  fetchMovieTitle(movieId);
+}
 
-request.get(apiUrl, (err, response, body) => {
-  if (err) {
-    console.error(err);
-  } else {
-    if (response.statusCode === 200) {
-      const Data = JSON.parse(body);
-      console.log(Data.title);
-  }
-});
