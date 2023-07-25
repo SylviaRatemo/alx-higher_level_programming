64f1027386ef09873906816450b9a7d926f3991d
SylviaRatemo <ratemosylvia@gmail.com>
2023-07-25 11:39:58 -0700

    count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,14 @@
 #!/usr/bin/node
-// character check
+// makes get request for SW movie id
 const request = require('request');
-
-const url = `${process.argv[2]}`;
-const id = '/18/';
-request(url, (err, res, body) => {
-        if (err) {
-                return console.log(err);
-        }
-	let count = 0;
-        const data = JSON.parse(body);
-	for (const film of data.results) {
-		for (const character of film.characters) {
-			count += (character.includes(id) ? 1 :0);
-		}
-	}
-        console.log(count);
+const find = '/18/';
+request(process.argv[2], function (error, response, body) {
+  if (error) throw new Error(error);
+  let num = 0;
+  for (const film of JSON.parse(body).results) {
+    for (const character of film.characters) {
+      num += (character.includes(find) ? 1 : 0);
+    }
+  }
+  console.log(num);
 });
