431918992e81712382df7273bf4d69891d09c9f3
SylviaRatemo <ratemosylvia@gmail.com>
2023-07-25 09:38:21 -0700

    writing into file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,20 +1,6 @@
 #!/usr/bin/node
-// read file content
+// write file passed as arg
 const fs = require('fs');
-function readAndPrintFile(filePath) {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(data);
-    }
-  });
-}
-
-// Usage
-if (process.argv.length < 3) {
-  console.error("Please provide the file path as a command-line argument.");
-} else {
-  const filePath = process.argv[2];
-  readAndPrintFile(filePath);
-}
+fs.writeFile(process.argv[2], process.argv[3], (err) => {
+  err && console.log(err);
+});
